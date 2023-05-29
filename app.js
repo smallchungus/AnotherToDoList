aedea9ac4d9b659faf258cfcd679068621a830f5
WillMACOS <wchen1396@gmail.com>
2023-05-29 11:21:59 -0400

    adding delete button to to do list tasks

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const addButton = document.getElementById("addButton");
 const taskList = document.getElementById("taskList");
 
 addButton.addEventListener("click", addTask);
+taskList.addEventListener("click", deleteTask);
 
 function addTask() {
     const taskText = taskInput.value.trim();
@@ -10,8 +11,20 @@ function addTask() {
     if (taskText !== "") {
         const li = document.createElement("li");
         li.innerText = taskText;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+
+        li.append(deleteButton);
         taskList.appendChild(li);
         taskInput.value = "";
 
+    }
+}
+
+function deleteTask(event) {
+    if (event.target.tagName === "BUTTON") {
+        const listItem = event.target.parentElement;
+        taskList.removeChild(listItem);
     }
 }
