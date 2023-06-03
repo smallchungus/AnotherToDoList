0c71a2266cb3f2f21dd33d70ce86fd1c171565ca
WillMACOS <wchen1396@gmail.com>
2023-06-03 12:51:03 -0400

    adding comments and making sure the delete button and the deadline works correctly.

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
+// get references to the HTML elements
 const taskInput = document.getElementById("taskInput");
+const deadlineInput = document.getElementById("deadlineInput")
 const addButton = document.getElementById("addButton");
 const taskList = document.getElementById("taskList");
 
+//add event listeners to the HTML elements
 addButton.addEventListener("click", addTask);
 taskList.addEventListener("click", deleteTask);
 
+//funciton to add task to the list
 function addTask() {
+    // get the task text and the deadline text
     const taskText = taskInput.value.trim();
+    const deadlineText = deadlineInput.value.trim();
 
+    // check if the task text is not empty
     if (taskText !== "") {
+        //create a new list item
         const li = document.createElement("li");
-        li.innerText = taskText;
 
+        //create a new label for the new task list item
+        const taskLabel = document.createElement("span");
+        taskLabel.innerText = taskText;
+
+        //create a new span element for the deadline
+        const deadlineLabel = document.createElement("span");
+        deadlineLabel.innerText = "Deadline: " + deadlineText;
+
+        // create a delete button for the new task list item
         const deleteButton = document.createElement("button");
         deleteButton.innerText = "Delete";
 
-        li.append(deleteButton);
+        // append the task label, deadline label, and the delete button to the new list item.
+        li.appendChild(taskLabel);
+        li.appendChild(deadlineInput);
+        li.appendChild(deleteButton);
         taskList.appendChild(li);
-        taskInput.value = "";
 
+        // reset the input fields to empty
+        taskInput.value = "";
+        deadlineInput.value = "";
     }
 }
 
@@ -27,4 +48,10 @@ function deleteTask(event) {
         const listItem = event.target.parentElement;
         taskList.removeChild(listItem);
     }
+}
+
+function formatDate ( dateString ) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric'};
+    const date = new Date(dateString);
+    return date.toLocaleDateString(undefined, options);
 }
